6a5b48183263487e826630e69845bb8587840251
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-09-10 12:35:05 +0500

    sale purchase agrement

---
+++ ol_sales_agreement_report/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Sale Purchase Agrement",
+
+    "author": "Cognative",
+    "category": "CRM",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": [
+        'sale'
+    ],
+
+    "data": [
+        'security/ir.model.access.csv',
+        'views/main_view.xml',
+        'reports/sale_agrement.xml',
+        'reports/sale_agrement_template.xml',
+        'reports/report.xml',
+        'reports/ali_sale_agreement.xml',
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+    "price": "60",
+    "currency": "EUR",
+}
